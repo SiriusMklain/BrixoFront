099612bad62e443cd6b639d7b222190464eb4828
qb <s.forostianyi@gmail.com>
2022-08-28 10:08:42 +0200

    Added axios and API for Articles to HomePage

--- src/App.js
+++ src/App.js
@@ -2,27 +2,35 @@ import './App.scss';
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Edit from "./pages/Edit/Edit";
-import React from "react";
+import React, {Component} from "react";
 import Header from "./components/Header/Header";
 
-function App() {
-  return (
-      <Router>
-        <div className="wrapper">
-          <Header/>
-          <div className="content">
-            <Routes>
-              <Route path="/">
-                <Route index element={<Home/>}/>
-                <Route path="edit" element={<Edit/>}/>
-              </Route>
-            </Routes>
-          </div>
-        </div>
-      </Router>
 
+class App extends Component {
 
-  );
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+
+    render() {
+        return (
+            <Router>
+                <div className="wrapper">
+                    <Header/>
+                    <div className="content">
+                        <Routes>
+                            <Route path="/">
+                                <Route index element={<Home/>}/>
+                                <Route path="edit" element={<Edit/>}/>
+                            </Route>
+                        </Routes>
+                    </div>
+                </div>
+            </Router>
+        )
+    }
 }
+
 
 export default App;
---
+++ src/util/ApiService.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+const API_URL =  "http://localhost:8000"
+
+class ApiService {
+
+    getArticles(){
+        const URL = `${API_URL}/api/v1/article/`;
+        return axios({
+            method: "GET",
+            url: URL,
+            headers: {
+                'content-type': 'application/json',
+            },
+            credentials: 'include',
+        }).then((response) => response.data);
+    }
+
+}
+
+export default ApiService;
