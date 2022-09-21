9e880d2825e02360ce6735faf1bd412c4359b3c3
foros <s.forostianyi@gmail.com>
2022-09-21 13:31:20 +0200

    Fixed deleteArticle

--- src/util/ApiService.js
+++ src/util/ApiService.js
@@ -105,6 +105,18 @@ class ApiService {
         }).then((response) => response.data);
     }
 
+    deleteArticle (pk) {
+        const URL = `${API_URL}/api/v1/article/${pk}/`;
+        return axios({
+            method: "DELETE",
+            url: URL,
+            headers: {
+                'content-type': 'application/json',
+            },
+            credentials: 'include',
+        }).then((response) => response.data);
+    }
+
     Article(pk) {
         const URL = `${API_URL}/api/v1/article/${pk}/`;
         return axios({
